4f7fea5aed2c17c817501e3379262295251d517b
renkeles <84261162+renkeles@users.noreply.github.com>
2021-08-10 23:04:22 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-int finalShift(int size, int shift){
-    bool flag = 0;
-    if(shift < 0){
-        shift = abs(shift);
-        flag = 1;
-    }
-    if(shift > size){
-        if(flag){
-            return 0 - abs(shift % size);
-        }
-        return abs(shift % size);
-    } else if(flag){
-        return 0 - shift;
-    } else{
-        return shift;
-    }
-}
-
-void shiftArray(char* lang, int size, int shift){
-    if(shift != 0){
-        char tmp = '\0';
-        int finShift = finalShift(size, shift);
-        if(finShift > 0){
-            
-        }else if(finShift < 0){
-
-        }
-    }
-}
-
 void toUppers(char* lang, int size){
     for(size_t i = 0; i < size; i++){
         if((int)lang[i] >= 97 && (int)lang[i] <= 122){
@@ -41,28 +11,68 @@ void toUppers(char* lang, int size){
 
 void print(char* lang, int size){
     for(size_t i = 0; i < size; i++){
+        //std::cout.width(3);
         std::cout << lang[i];
     }
     std::cout << std::endl;
 }
 
 void cesarSH(char* lang, int size, int key){
-    //toUppers(lang, size);
-    shiftArray(lang, size, key);
+    toUppers(lang, size);
+    char ABC[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    int ABCSize = sizeof(ABC) / sizeof(char) - 1;
+    key = abs(key % ABCSize);
+    int tmp;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < ABCSize; j++){
+            if(lang[i] == ABC[j]){
+                if(j + key > ABCSize - 1){
+                    tmp = ((j + key) % ABCSize);
+                    lang[i] = ABC[tmp];
+                    break;
+                }
+                tmp = j;
+                lang[i] = ABC[tmp + key];
+                break;
+            }
+        }
+    }
+}
+
+void deCesarSH(char* lang, int size, int key){
+    toUppers(lang, size);
+    char ABC[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    int ABCSize = sizeof(ABC) / sizeof(char) - 1;
+    key = abs(key % ABCSize);
+    int tmp;
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < ABCSize; j++){
+            if(lang[i] == ABC[j]){
+                if(j < key){
+                    tmp = ABCSize - key + j;
+                    lang[i] = ABC[tmp];
+                    break;
+                }
+                tmp = j - key;
+                lang[i] = ABC[tmp];
+                break;
+            }
+        }
+    }
 }
 
 int main() {
 
-    char string[] = {"This is string? Yes, it's string"};
-    int key = -3;
-    int size = sizeof(string) / sizeof(char);
+    char string[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ ZYXWVUTSRQPONMLKJIHGFEDCBA"};
+    int key = 5;
+    int size = sizeof(string) / sizeof(char) - 1;
     print(string, size);
     cesarSH(string, size, key);
     print(string, size);
-
+    deCesarSH(string, size, key);
+    print(string, size);
 
 
 
     return 0;
 }
-
